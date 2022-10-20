7a0d828f364550396fd61e11c7de6fe8f1db19d0
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-10-20 20:56:02 +0600

    Post Loop Creation

--- template-part/content-testimonial.php
+++ template-part/content-testimonial.php
@@ -11,6 +11,73 @@
                 </div>
             </div>
             <div class="row">
+
+            <?php
+
+            // the query (for Post Loop)
+
+            $args = array(
+                'post_type' => 'customservice'
+            );
+
+            $the_query = new WP_Query( $args ); ?>
+
+            <?php if ( $the_query->have_posts() ) : ?>
+
+                <!-- pagination here -->
+
+                <!-- the loop -->
+                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); 
+                $image_url;
+                // Image Location.
+                if ( has_post_thumbnail() ) {
+                    $image_url = get_the_post_thumbnail_url(get_the_ID());
+                }
+                else {
+                    $image_url = get_bloginfo('stylesheet_directory') .'/img/about-img.jpg"';
+                }
+                
+                ?>
+                <!-- HTML Code Design -->
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="service-block">
+                            <!-- service block -->
+                            <div class="service-icon mb20">
+                                <!-- service img -->
+                                <img src="<?php echo $image_url; ?>" alt=""> </div>
+                            <!-- service img -->
+                            <div class="service-content">
+                                <!-- service content -->
+                                <h2><a href="# " class="title "><?php the_title(); ?></a></h2>
+                                <p><?php the_content(); ?></p>
+                                <?php
+                                    $postid = $the_query->post->ID;
+
+                                    $price = get_post_meta($postid, 'Price', true);
+                                ?>
+                                <div class="price "><?php echo $price; ?></div>
+                            </div>
+                            <!-- service content -->
+                        </div>
+                        <!-- /.service block -->
+                </div>
+
+                    
+                <?php endwhile; ?>
+                <!-- end of the loop -->
+
+                <!-- pagination here -->
+
+                <?php wp_reset_postdata(); ?>
+
+                <?php else : ?>
+                    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                <?php endif; 
+                // End the query (for Post Loop) 
+
+                ?>
+
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                     <div class="testimonial-block">
                         <!-- testimonial block -->
