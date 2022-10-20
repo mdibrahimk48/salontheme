5c7982e3e58386cbdef68c015970cdf319ec7936
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-10-20 17:47:31 +0600

    Making Dynamic

--- template-part/content-services.php
+++ template-part/content-services.php
@@ -11,6 +11,8 @@
                 </div>
             </div>
             <div class="row">
+
+            
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                     <div class="service-block">
                         <!-- service block -->
